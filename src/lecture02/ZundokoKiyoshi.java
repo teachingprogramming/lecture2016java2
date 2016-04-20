28850e2320753e7f70dad59e109df96b8d43ffaf
teachingprogramming <teacher@teachingprogramming.net>
2016-04-21 08:08:11 +0900

    lecture02: ZundokoKiyoshi

---
+++ src/lecture02/ZundokoKiyoshi.java
@@ -0,0 +1,25 @@
+package lecture02;
+
+/**
+ * ズンドコキヨシ
+ * http://teachingprogramming.net/
+ */
+public class ZundokoKiyoshi {
+    public static void main(String[] args) {
+        int zun_count = 0;
+        while (true) {
+            double random = Math.random();
+            if (random < 0.7) {
+                System.out.println("ズン");
+                zun_count++;
+            } else {
+                System.out.println("ドコ");
+                if (zun_count >= 4) {
+                    System.out.println("キ・ヨ・シ！");
+                    break;
+                }
+                zun_count = 0;
+            }
+        }
+    }
+}
