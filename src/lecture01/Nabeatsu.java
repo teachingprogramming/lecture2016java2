de51b80ae29d3822e461bf90faba684cd844cbf9
teachingprogramming <teacher@teachingprogramming.net>
2016-04-20 09:36:12 +0900

    lecture01: Nabeatsu

---
+++ src/lecture01/Nabeatsu.java
@@ -0,0 +1,16 @@
+package lecture01;
+
+/**
+ * 3の倍数でアホになる（ベースとなるプログラム）
+ */
+public class Nabeatsu {
+    public static void main(String[] args) {
+        for (int i=1; i<=100; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i + "!");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
