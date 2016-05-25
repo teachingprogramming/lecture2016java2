f7a8f908edd9f906a925946642ce2310bca22dd7
teachingprogramming <teacher@teachingprogramming.net>
2016-05-25 10:18:11 +0900

    lecture04: コンストラクタ、アクセス修飾子

---
+++ src/net/teachingprogramming/lecture04/Main.java
@@ -0,0 +1,16 @@
+package net.teachingprogramming.lecture04;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("ドラえもん", 129.3, 1.293);
+        person1.selfIntroduction();
+        person1.name = "のび太"; // nameはprivateなのでエラーになる
+
+
+        Person person2 = new Person("ドラミ", 91.0, 1.0);
+        person2.selfIntroduction();
+
+        Person person3 = new Person(); // 引数なしのコンストラクタは用意されてないのでエラーになる。
+
+    }
+}
