88f57746f185e0c912f28f5424be02cb2cb8d23c
teachingprogramming <teacher@teachingprogramming.net>
2016-05-31 21:49:21 +0900

    lecture04: コンパイルできるようにエラーとなる部分はコメントアウト

--- src/net/teachingprogramming/lecture04/Main.java
+++ src/net/teachingprogramming/lecture04/Main.java
@@ -4,14 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Person person1 = new Person("ドラえもん", 129.3, 1.293);
         person1.selfIntroduction();
+        /*
         person1.name = "のび太"; // nameはprivateなのでエラーになる
-
+        */
 
         Person person2 = new Person("ドラミ", 91.0, 1.0);
         person2.selfIntroduction();
-
+        /*
         Person person3 = new Person(); // 引数なしのコンストラクタは用意されてないのでエラーになる。
-
+        */
         Teacher teacher1 = new Teacher("たなか", 70.0, 1.75);
         teacher1.selfIntroduction();
         teacher1.bmiHantei();
