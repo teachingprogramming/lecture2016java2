5b5a824a7936c73649aa3422b2a6721c7c18104d
teachingprogramming <teacher@teachingprogramming.net>
2016-06-09 04:26:35 +0900

    lecture07: 回答等

--- src/net/teachingprogramming/lecture07/p1/Main.java
+++ src/net/teachingprogramming/lecture07/p1/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Rectangle r = new Rectangle(10, 5);
         int area = r.getArea();
-        System.out.println("面積は" + area); //
+        System.out.println("面積は" + area);
         if (r.isSquare()) {
             System.out.println("正方形である。");
         } else {
