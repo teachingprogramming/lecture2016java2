293d9eecfb416df1e60605439740a1cc176bd398
teachingprogramming <teacher@teachingprogramming.net>
2016-06-18 17:14:34 +0900

    exam1: 回答例 fix

--- src/net/teachingprogramming/ans1/p2/Main.java
+++ src/net/teachingprogramming/ans1/p2/Main.java
@@ -21,8 +21,8 @@ public class Main {
             }
         }
         System.out.println(sum);
-        System.out.println(max);
         System.out.println(div3);
+        System.out.println(max);
         // 回答ここまで
     }
 
